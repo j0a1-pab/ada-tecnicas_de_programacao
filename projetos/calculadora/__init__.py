93fab01d1cc8c1abbbcbdb47b8d4b8bde201b7a6
j0a1-pab <bueno.joao@aluno.ufabc.edu.br>
2023-03-30 09:38:42 -0300

    Adicionada a função multiplicacao
     No ramo multiplicacao
     Mudanças a serem submetidas:
    	modified:   projetos/calculadora/__init__.py
    	modified:   projetos/calculadora/funcoes.py

--- projetos/calculadora/__init__.py
+++ projetos/calculadora/__init__.py
@@ -1,5 +1,8 @@
 from funcoes import soma
+from funcoes import multiplicacao
 
 def calcula(x, y, operacao):
     if operacao == "+":
         return soma(x, y)
+    elif operacao == "*":
+        return multiplicacao(x, y)
--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -3,3 +3,10 @@ def soma(a, b):
         return a + b
     else:
         raise TypeError(f"O input 'a' e 'b' devem ser uma string, recebido {a}, {type(a)}, {b}, {type(b)}.")
+    
+def multiplicacao(a, b):
+    if isinstance(a, (int, float)) or isinstance(b, (int, float)):
+        return a * b
+    else:
+        raise TypeError(f"O input 'a' e 'b' devem ser uma string, recebido {a}, {type(a)}, {b}, {type(b)}.")
+    
